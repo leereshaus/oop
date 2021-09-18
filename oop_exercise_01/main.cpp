fc1f1764d94dcda2328c82205b4d501d2d32f02e
leereshaus <90936484+leereshaus@users.noreply.github.com>
2021-09-18 10:50:52 +0300

    Create main.cpp

---
+++ oop_exercise_01/main.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include "cmath"
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+class Rectangle
+{
+private:
+    double x1, x2, y1, y2;
+
+public:
+    Rectangle(double x1, double y1, double x2, double y2)
+    {
+        this->x1 = x1;
+        this->y1 = y1;
+        this->x2 = x2;
+        this->y2 = y2;
+    }
+    void print()
+    {
+        std::cout << "(" << x1 << "," << y1 << "), " << "(" << x2 << "," << y2 << ")\n";
+    }
+    double getSquare()
+    {
+        return (x2-x1)*(y2-y1);
+    }
+    double getPerim()
+    {
+        return 2*(x2-x1+y2-y1);
+    }
+    void move(double a, double b)
+    {
+        x1 += a;
+        x2 += a;
+        y1 += b;
+        y2 += b;
+    }
+    void changeSize(double c, double d)
+    {
+        x2 = (x2-x1) * c + x1;
+        y2 = (y2-y1) * d + y1;
+    }
+    int compareBySq(Rectangle z)
+    {
+        if (getSquare() > z.getSquare())
+        {
+            return 1;
+        }
+        else if (getSquare() < z.getSquare())
+        {
+            return -1;
+        }
+        return 0;
+    }
+    int compareByPerim(Rectangle z)
+    {
+        if (getPerim() > z.getPerim())
+        {
+            return 1;
+        }
+        else if (getPerim() < z.getPerim())
+        {
+            return -1;
+        }
+        return 0;
+    }
+    Rectangle unite(Rectangle z)
+    {
+        double a1, b1, a2, b2;
+        a1 = min(min(x1, x2), min(z.x1, z.x2));
+        b1 = min(min(y1, y2), min(z.y1, z.y2));
+        a2 = max(max(x1, x2), max(z.x1, z.x2));
+        b2 = max(max(y1, y2), max(z.y1, z.y2));
+        return Rectangle(a1, b1, a2, b2);
+    }
+};
+
+Rectangle create()
+{
+    double a1, b1, a2, b2;
+    std::cout << "Введите координаты левого нижнего и правого верхнего углов прямоугольника\n";
+    std::cin >> a1 >> b1 >> a2 >> b2;
+    return Rectangle(a1, b1, a2, b2);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    Rectangle r(0, 0, 0, 0);
+    while(1)
+    {
+        std::cout << "1. Ввести координаты" << "\n" << "2. Вывести координаты" << "\n" << "3. Посчитать площадь" << "\n" << "4. Посчитать периметр" << "\n" << "5. Переместить вдоль осей" << "\n" << "6. Изменить размер" << "\n" << "7. Сравнить по площади и периметру" <<  "\n" << "8. Пересечение двух прямоугольников" << "\n" << "9. Объединение двух прямоугольников" << "\n" << "Выполнить операцию: " << "\t";
+        std::cin >> n;
+        std::cout << "\n";
+        if (n == 1)
+        {
+            r = create();
+            std::cout << "\n";
+        }
+        else if (n == 2)
+        {
+            std::cout << "Заданный прямоугольник имеет координаты: \t";
+            r.print();
+            std::cout << "\n";
+        }
+        else if (n == 3)
+        {
+            std::cout <<"Площадь прямоугольника = " << r.getSquare() << std::endl;
+            std::cout << "\n";
+        }
+        else if (n == 4)
+        {
+            std::cout << "Периметр прямоугольника = " << r.getPerim() << std::endl;
+            std::cout << "\n";
+        }
+        else if (n == 5)
+        {
+            double a, b;
+            std::cout << "Введите смещение (по оси x и по оси y) \n";
+            std::cin >> a >> b;
+            r.move(a, b);
+            r.print();
+            std::cout << "\n";
+        }
+        else if (n == 6)
+        {
+            double c, d;
+            std::cout << "Введите изменение (по оси x и по оси y) \n";
+            std::cin >> c >> d;
+            r.changeSize(c, d);
+            r.print();
+            std::cout << "\n";
+        }
+        else if (n == 7)
+        {
+            Rectangle s = create();
+            if (r.compareBySq(s) == 1)
+            {
+                std::cout << "The first rectangle is greater than the second by Square" << std::endl;
+            }
+            else if (r.compareBySq(s) == -1)
+            {
+                std::cout << "The first rectangle is smaller than the second by Square" << std::endl;
+            }
+            else
+            {
+                std::cout << "Squares are equal" << std::endl;
+            }
+            if (r.compareByPerim(s) == 1)
+            {
+                std::cout << "The first rectangle is greater than the second by Perimeter" << std::endl;
+            }
+            else if (r.compareByPerim(s) == -1)
+            {
+                std::cout << "The first rectangle is smaller than the second by Perimeter" << std::endl;
+            }
+            else
+            {
+                std::cout << "Perimeter are equal" << std::endl;
+            }
+            std::cout << "\n";
+        }
+        else if (n == 9)
+        {
+            std::cout << "Первый прямоугольник имеет координаты двух противоположных углов: " << "\t";
+            r.print();
+            std::cout << "\n";
+            Rectangle s = create();
+            std::cout << "Второй прямоугольник имеет координаты двух противоположных углов: " << "\t";
+            s.print();
+            Rectangle p = r.unite(s);
+            std::cout << "Наименьший прямоугольник, составленный путем объединения двух других, имеет координаты двух противоположных углов: " << "\t";
+            p.print();
+            std::cout << "\n";
+        }
+    }
+    return 0;
+}
